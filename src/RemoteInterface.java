985e4156d648cba1cfef6ecfadff748b20e409ac
qrkwang <1901829@sit.singaporetech.edu.sg>
2021-06-21 18:08:31 +0800

    addition to fix error

--- src/RemoteInterface.java
+++ src/RemoteInterface.java
@@ -1,6 +1,7 @@
+import java.rmi.Remote;
 import java.rmi.RemoteException;
 
-public interface RemoteInterface {
+public interface RemoteInterface extends Remote {
 	public String testMethod(String s) throws RemoteException;
 
 }
--- src/RemoteServant.java
+++ src/RemoteServant.java
@@ -1,6 +1,12 @@
 import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
 
-public class RemoteServant implements RemoteInterface {
+public class RemoteServant extends UnicastRemoteObject implements RemoteInterface {
+	public RemoteServant() throws RemoteException {
+		super();
+		System.out.format("Creating server object\n"); // Print to client that server object is being created once
+														// constructor called.
+	}
 
 	@Override
 	public String testMethod(String s) throws RemoteException {
