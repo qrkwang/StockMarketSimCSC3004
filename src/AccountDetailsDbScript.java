e9fcd90c356ca736ddf031e90215ada6e71ea77e
qrkwang <1901829@sit.singaporetech.edu.sg>
2021-06-21 18:46:51 +0800

    update jdbc

--- src/AccountDetailsDbScript.java
+++ src/AccountDetailsDbScript.java
@@ -1,16 +1,28 @@
 import java.sql.CallableStatement;
-import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import com.mysql.jdbc.Connection;
+
 //Put db connection and queries for AccountDetails db here.
 
 public class AccountDetailsDbScript {
+	public static final String DRIVER_CLASS = "com.mysql.jdbc.Driver";
+	private static final String USERNAME = "root";
+	private static final String PASSWORD = "password";
+	private static final String CONN_STRING = "jdbc:mysql://localhost:3306/test"; // jdbc:mysql://ip:3306/DBNAME
 
 	public static void getAccountDetails(int accountDetailsId) throws SQLException {
-
-		Connection con = DriverManager.getConnection("jdbc:default:connection");
+		Connection con = null;
+		try {
+			Class.forName(DRIVER_CLASS);
+			con = (Connection) DriverManager.getConnection(CONN_STRING, USERNAME, PASSWORD);
+			System.out.println("Connected to DB");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 		String query = "{CALL get_account_Details(?)}"; // Query of calling stored procedure "Get account Details" with
 														// input of ?
@@ -21,14 +33,18 @@ public class AccountDetailsDbScript {
 		ResultSet rs = stmt.executeQuery();
 
 		while (rs.next()) {
-			System.out.println(String.format("%s - %s", rs.getString("first_name") + " " + rs.getString("last_name"), //Getting attribute of returned rows and printing it.
+			System.out.println(String.format("%s - %s", rs.getString("first_name") + " " + rs.getString("last_name"), // Getting
+																														// attribute
+																														// of
+																														// returned
+																														// rows
+																														// and
+																														// printing
+																														// it.
 					rs.getString("skill")));
 		}
 		con.close();
-		//When really do the method, will have a return value to server for it to use.
+		// When really do the method, will have a return value to server for it to use.
 	}
-	
-	
-	
-	
+
 }
