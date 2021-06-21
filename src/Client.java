7a476b96b059663beebd79d674b4fc31f780fd8b
qrkwang <1901829@sit.singaporetech.edu.sg>
2021-06-21 21:25:12 +0800

    filler methods and client calls

--- src/Client.java
+++ src/Client.java
@@ -1,16 +1,18 @@
 import java.rmi.Naming;
 import java.rmi.RemoteException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Client extends java.rmi.server.UnicastRemoteObject implements ClientInt {
-
 	public Client() throws RemoteException {
 
 	}
 
+	private static int accountId;
+
 	// Client will need to display account details, send buy/sell order, list of
 	// account stock holdings, polling of stock price per interval (not necessarily
-	// need polling, can be on update) when on that page.
+	// need polling, can be on update) when on that page, stock page orders.
 
 	public static void main(String[] args) {
 		System.out.println("client main method");
@@ -18,11 +20,28 @@ public class Client extends java.rmi.server.UnicastRemoteObject implements Clien
 		try {
 			RemoteInterface remoteObj = (RemoteInterface) Naming.lookup("rmi://localhost:1099/RemoteServer");
 
+			// Interface for login
+			String username = "demo";
 			Scanner stdin = new Scanner(System.in); // Init scanner
 			String input = null;
 			String secondInput = null;
 
 			remoteObj.testMethod("test method");
+			String accountDetails = remoteObj.getAccountDetailsByUsername(username);
+			// Will receive a string with account details delimited by "," . First item is
+			// accountId. Assign accountId value to global variable so can use on other
+			// methods.
+			String[] splitArray = accountDetails.split(",");
+			accountId = Integer.parseInt(splitArray[0]);
+			remoteObj.getAccountHoldingsById(accountId);
+
+			// retrieve stock per market, retrieve orders per stock, retrieve price per
+			// stock
+			String stockPrice = remoteObj.retrievePrice("SG", "SGX:A17U");
+			ArrayList stockOrderList = remoteObj.retrieveOrders("SG", "SGX:A17U");
+
+			String stockPrice1 = remoteObj.retrievePrice("US", "NASDAQ:AAPL");
+			ArrayList stockOrderList1 = remoteObj.retrieveOrders("US", "NASDAQ:AAPL");
 
 		} catch (Exception e) {
 			System.out.format("Error obtaining remoteServer/remoteInterface from registry");
--- src/RemoteInterface.java
+++ src/RemoteInterface.java
@@ -1,7 +1,22 @@
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.ArrayList;
 
 public interface RemoteInterface extends Remote {
+	// Client will need to display account details, send buy/sell order, list of
+	// account stock holdings, polling of stock price per interval (not necessarily
+	// need polling, can be on update) when on that page, stock page orders.
+
 	public String testMethod(String s) throws RemoteException;
+
+	public String getAccountDetailsByUsername(String username) throws RemoteException;
+
+	public ArrayList getAccountHoldingsById(int accountId) throws RemoteException;
+
+	public String sendOrder(String accountId, String order) throws RemoteException;
+
+	public ArrayList retrieveOrders(String market, String tickerSymbol) throws RemoteException;
+
+	public String retrievePrice(String market, String tickerSymbol) throws RemoteException;
 
 }
