b7ecf7d1f168d172eb532044a4646e1fa5530a21
qrkwang <1901829@sit.singaporetech.edu.sg>
2021-06-21 21:25:52 +0800

    Update RemoteServant.java

--- src/RemoteServant.java
+++ src/RemoteServant.java
@@ -1,5 +1,6 @@
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
 
 public class RemoteServant extends UnicastRemoteObject implements RemoteInterface {
 	public RemoteServant() throws RemoteException {
@@ -12,6 +13,36 @@ public class RemoteServant extends UnicastRemoteObject implements RemoteInterfac
 	public String testMethod(String s) throws RemoteException {
 		System.out.println(s);
 		return "printed";
+	}
+
+	@Override
+	public String getAccountDetailsByUsername(String username) throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList getAccountHoldingsById(int accountId) throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String sendOrder(String accountId, String order) throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList retrieveOrders(String market, String tickerSymbol) throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String retrievePrice(String market, String tickerSymbol) throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
