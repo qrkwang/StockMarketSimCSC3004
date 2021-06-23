e1352cd8064081e52c903338e4077067ca2e91d2
Joy Tan <weiling22@hotmail.com>
2021-06-23 18:21:35 +0800

    Update create db script

--- sqls/CreateSGDb.sql
+++ sqls/CreateSGDb.sql
@@ -15,35 +15,35 @@ CREATE TABLE `stock` (
   UNIQUE KEY `StockId_UNIQUE` (`StockId`)
 );
 
-DROP TABLE IF EXISTS `marketrow`;
-CREATE TABLE `marketrow` (
-  `MarketRowId` int(11) NOT NULL AUTO_INCREMENT,
-  `StockId` int(11) NOT NULL,
-  `IsBuying`  BOOLEAN NOT NULL,
-  `Quantity` int(11) NOT NULL,
-  `Price` double NOT NULL,
-  PRIMARY KEY (`MarketRowId`),
-  UNIQUE KEY `MarketRowId_UNIQUE` (`MarketRowId`)
- 
-);
-
-DROP TABLE IF EXISTS `marketorders`;
-CREATE TABLE `marketorders` (
-  `MarketOrdersId` int(11) NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS `marketpending`;
+CREATE TABLE `marketpending` (
+  `MarketPendingId` int(11) NOT NULL AUTO_INCREMENT,
   `StockId` int(11) NOT NULL,
   `SellerId` int(11) DEFAULT NULL,
   `BuyerId` int(11) DEFAULT NULL,
   `Quantity` int(11) NOT NULL,
-  `MarketRowId` int(11) NOT NULL,
   `Price` double NOT NULL,
-  `ConfirmedPrice` double NOT NULL,
-  `IsClosed`  BOOLEAN NOT NULL,
-  `TransactionDate` datetime NOT NULL,
-  PRIMARY KEY (`MarketOrdersId`),
-  UNIQUE KEY `MarketOrdersId_UNIQUE` (`MarketOrdersId`),
-  CONSTRAINT FK_StockId  FOREIGN KEY(StockId)  REFERENCES stock(StockId) ON DELETE CASCADE,
-  CONSTRAINT FK_MarketRowId  FOREIGN KEY(MarketRowId)  REFERENCES marketrow(MarketRowId) ON DELETE CASCADE
+  `CreatedDate` datetime NOT NULL,
+  PRIMARY KEY (`MarketPendingId`),
+  UNIQUE KEY `MarketPendingId_UNIQUE` (`MarketPendingId`),
+  CONSTRAINT FK_PendingStockId  FOREIGN KEY(StockId)  REFERENCES stock(StockId) ON DELETE CASCADE
 );
+
+DROP TABLE IF EXISTS `marketcompleted`;
+CREATE TABLE `marketcompleted` (
+  `MarketCompletedId` int(11) NOT NULL AUTO_INCREMENT,
+  `StockId` int(11) NOT NULL,
+  `SellerId` int(11) DEFAULT NULL,
+  `BuyerId` int(11) DEFAULT NULL,
+  `Quantity` int(11) NOT NULL,
+  `Price` double NOT NULL,
+  `TransactionDate` datetime NOT NULL,
+  PRIMARY KEY (`MarketCompletedId`),
+  UNIQUE KEY `MarketCompletedId_UNIQUE` (`MarketCompletedId`),
+  CONSTRAINT FK_CompletedStockId  FOREIGN KEY(StockId)  REFERENCES stock(StockId) ON DELETE CASCADE
+);
+
+
 
 /*data*/
 /*Status true = open, false = closed */
@@ -54,17 +54,16 @@ INSERT INTO `stock` VALUES (null,'Oversea-Chinese Banking Corp. Limited','SGX: O
 INSERT INTO `stock` VALUES (null,'Singapore Telecommunications Limited','SGX: Z74',2.30,true,'GMT+8',current_timestamp());
 Select * from stock;
 
-INSERT INTO `marketrow` VALUES (1,1,true,12,12.12);
-INSERT INTO `marketrow` VALUES (null,1,true,12,12.38);
-INSERT INTO `marketrow` VALUES (null,2,false,14,14.48);
-INSERT INTO `marketrow` VALUES (null,3,false,13,13.36);
-INSERT INTO `marketrow` VALUES (null,3,false,13,13.47);
-Select * from marketrow;
+INSERT INTO `marketpending` VALUES (1,1,1,null,12,12.38,current_timestamp());
+INSERT INTO `marketpending` VALUES (null,1,null,4,12,12.38,current_timestamp());
+INSERT INTO `marketpending` VALUES (null,2,3,null,12,2.76,current_timestamp());
+INSERT INTO `marketpending` VALUES (null,3,4,null,12,11.53,current_timestamp());
+INSERT INTO `marketpending` VALUES (null,3,null,5,12,19.80,current_timestamp());
+Select * from marketpending;
 
-
-INSERT INTO `marketorders` VALUES (1,1,4,null,12,1,12.38,13.38,true,current_timestamp());
-INSERT INTO `marketorders` VALUES (null,1,3,null,12,1,12.38,13.38,true,current_timestamp());
-INSERT INTO `marketorders` VALUES (null,2,3,null,12,1,2.76,13.38,false,current_timestamp());
-INSERT INTO `marketorders` VALUES (null,3,4,null,12,1,11.53,13.38,false,current_timestamp());
-INSERT INTO `marketorders` VALUES (null,3,4,5,12,1,19.80,19.38,false,current_timestamp());
-Select * from marketorders;
+INSERT INTO `marketcompleted` VALUES (1,1,1,2,12,12.38,current_timestamp());
+INSERT INTO `marketcompleted` VALUES (null,1,3,4,12,12.38,current_timestamp());
+INSERT INTO `marketcompleted` VALUES (null,2,3,4,12,2.76,current_timestamp());
+INSERT INTO `marketcompleted` VALUES (null,3,4,3,12,11.53,current_timestamp());
+INSERT INTO `marketcompleted` VALUES (null,3,5,5,12,19.80,current_timestamp());
+Select * from marketcompleted;
--- sqls/CreateUSDb.sql
+++ sqls/CreateUSDb.sql
@@ -14,34 +14,32 @@ CREATE TABLE `stock` (
   PRIMARY KEY (`StockId`),
   UNIQUE KEY `StockId_UNIQUE` (`StockId`)
 );
-DROP TABLE IF EXISTS `marketrow`;
-CREATE TABLE `marketrow` (
-  `MarketRowId` int(11) NOT NULL AUTO_INCREMENT,
-  `StockId` int(11) NOT NULL,
-  `IsBuying`  BOOLEAN NOT NULL,
-  `Quantity` int(11) NOT NULL,
-  `Price` double NOT NULL,
-  PRIMARY KEY (`MarketRowId`),
-  UNIQUE KEY `MarketRowId_UNIQUE` (`MarketRowId`)
- 
-);
-
-DROP TABLE IF EXISTS `marketorders`;
-CREATE TABLE `marketorders` (
-  `MarketOrdersId` int(11) NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS `marketpending`;
+CREATE TABLE `marketpending` (
+  `MarketPendingId` int(11) NOT NULL AUTO_INCREMENT,
   `StockId` int(11) NOT NULL,
   `SellerId` int(11) DEFAULT NULL,
   `BuyerId` int(11) DEFAULT NULL,
   `Quantity` int(11) NOT NULL,
-  `MarketRowId` int(11) NOT NULL,
   `Price` double NOT NULL,
-  `ConfirmedPrice` double NOT NULL,
-  `IsClosed`  BOOLEAN NOT NULL,
+  `CreatedDate` datetime NOT NULL,
+  PRIMARY KEY (`MarketPendingId`),
+  UNIQUE KEY `MarketPendingId_UNIQUE` (`MarketPendingId`),
+  CONSTRAINT FK_PendingStockId  FOREIGN KEY(StockId)  REFERENCES stock(StockId) ON DELETE CASCADE
+);
+
+DROP TABLE IF EXISTS `marketcompleted`;
+CREATE TABLE `marketcompleted` (
+  `MarketCompletedId` int(11) NOT NULL AUTO_INCREMENT,
+  `StockId` int(11) NOT NULL,
+  `SellerId` int(11) DEFAULT NULL,
+  `BuyerId` int(11) DEFAULT NULL,
+  `Quantity` int(11) NOT NULL,
+  `Price` double NOT NULL,
   `TransactionDate` datetime NOT NULL,
-  PRIMARY KEY (`MarketOrdersId`),
-  UNIQUE KEY `MarketOrdersId_UNIQUE` (`MarketOrdersId`),
-  CONSTRAINT FK_StockId  FOREIGN KEY(StockId)  REFERENCES stock(StockId) ON DELETE CASCADE,
-  CONSTRAINT FK_MarketRowId  FOREIGN KEY(MarketRowId)  REFERENCES marketrow(MarketRowId) ON DELETE CASCADE
+  PRIMARY KEY (`MarketCompletedId`),
+  UNIQUE KEY `MarketCompletedId_UNIQUE` (`MarketCompletedId`),
+  CONSTRAINT FK_CompletedStockId  FOREIGN KEY(StockId)  REFERENCES stock(StockId) ON DELETE CASCADE
 );
 
 
@@ -54,17 +52,16 @@ INSERT INTO `stock` VALUES (null,'Netflix','NASDAQ: NFLX',498.01,true,'GMT-4',cu
 INSERT INTO `stock` VALUES (null,'Walt Disney Co','NYSE: DIS: AAPL',174.30,true,'GMT-4',current_timestamp());
 Select * from stock;
 
-INSERT INTO `marketrow` VALUES (1,1,true,12,12.12);
-INSERT INTO `marketrow` VALUES (null,1,true,12,12.38);
-INSERT INTO `marketrow` VALUES (null,2,false,14,14.48);
-INSERT INTO `marketrow` VALUES (null,3,false,13,13.36);
-INSERT INTO `marketrow` VALUES (null,3,false,13,13.47);
-Select * from marketrow;
+INSERT INTO `marketpending` VALUES (1,1,1,null,12,12.38,current_timestamp());
+INSERT INTO `marketpending` VALUES (null,1,null,4,12,12.38,current_timestamp());
+INSERT INTO `marketpending` VALUES (null,2,3,null,12,2.76,current_timestamp());
+INSERT INTO `marketpending` VALUES (null,3,4,null,12,11.53,current_timestamp());
+INSERT INTO `marketpending` VALUES (null,3,null,5,12,19.80,current_timestamp());
+Select * from marketpending;
 
-
-INSERT INTO `marketorders` VALUES (1,1,4,null,12,1,12.38,13.38,true,current_timestamp());
-INSERT INTO `marketorders` VALUES (null,1,3,null,12,1,12.38,13.38,true,current_timestamp());
-INSERT INTO `marketorders` VALUES (null,2,3,null,12,1,2.76,13.38,false,current_timestamp());
-INSERT INTO `marketorders` VALUES (null,3,4,null,12,1,11.53,13.38,false,current_timestamp());
-INSERT INTO `marketorders` VALUES (null,3,4,5,12,1,19.80,19.38,false,current_timestamp());
-Select * from marketorders;
+INSERT INTO `marketcompleted` VALUES (1,1,1,2,12,12.38,current_timestamp());
+INSERT INTO `marketcompleted` VALUES (null,1,3,4,12,12.38,current_timestamp());
+INSERT INTO `marketcompleted` VALUES (null,2,3,4,12,2.76,current_timestamp());
+INSERT INTO `marketcompleted` VALUES (null,3,4,3,12,11.53,current_timestamp());
+INSERT INTO `marketcompleted` VALUES (null,3,5,5,12,19.80,current_timestamp());
+Select * from marketcompleted;
