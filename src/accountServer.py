fd1b14ac48279ef193f4cfd44f4c0a82c2b86db6
probu <1901877@sit.singaporetech.edu.sg>
2021-06-28 13:38:17 +0800

    update working script
    still incomplete

--- src/accountServer.py
+++ src/accountServer.py
@@ -1,19 +1,23 @@
 import paramiko
 
-host = "192.168.87.56"
+host = "192.168.87.55"
 username = "wh1901877"
-#password = "password"
-command = "sudo /etc/init.d/mysql start"
+command = "sudo systemctl restart mysql"
 
 try:
     ssh_client = paramiko.SSHClient()
     ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh_client.connect(hostname=host, username=username)
     print("Connected to Account Server")
-    tdin,stdout,stderr=ssh_client.exec_command(command)
+    stdin,stdout,stderr=ssh_client.exec_command(command)
+    print("Connected to Account Server testing")
     for result in stdout.readlines():
         print("command manage to run" + result)
     if stderr != "":
         print(stderr)
 except:
     print("error")
+
+
+
+#https://phpraxis.wordpress.com/2016/09/27/enable-sudo-without-password-in-ubuntudebian/
