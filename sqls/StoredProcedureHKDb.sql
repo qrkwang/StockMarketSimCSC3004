df7d7015ffb79419e074adc5c21df584aaca78c3
Joy Tan <weiling22@hotmail.com>
2021-07-10 16:40:27 +0800

    updated Stored procedure

--- sqls/StoredProcedureHKDb.sql
+++ sqls/StoredProcedureHKDb.sql
@@ -38,12 +38,12 @@ DELIMITER $$
 USE `HKStockMarket`$$
 
 CREATE PROCEDURE `InsertToMarketPending`(
-IN stockId Int, In sellerId Int, In buyerId Int, In quantity Int, In price Double, In transactionDate DATETIME
+IN stockId Int, In sellerId Int, In buyerId Int, In quantity Int, In price Double, In createdDate DATETIME
 )
 BEGIN
 
-INSERT INTO marketPending (StockId, SellerId, BuyerId, Quantity, Price, TransactionDate)
-VALUES(stockId,sellerId, buyerId, quantity,price, transactionDate);
+INSERT INTO marketPending (StockId, SellerId, BuyerId, Quantity, Price, CreatedDate)
+VALUES(stockId,sellerId, buyerId, quantity,price, createdDate);
 
 END$$
 DELIMITER ;
