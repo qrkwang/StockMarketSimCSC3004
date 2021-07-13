ce1d63c9c1d102c94578a2ddfa03c207d7afbd6e
probu <1901877@sit.singaporetech.edu.sg>
2021-07-13 23:46:27 +0800

    update script

--- sqls/StoredProcedureAccountDetails.sql
+++ sqls/StoredProcedureAccountDetails.sql
@@ -1,9 +1,9 @@
-#USE AccountDetailsServer;  
+USE AccountDetailsServer;  
 DROP procedure IF EXISTS `getAccountDetailsByUsername`;
 
 DELIMITER $$
 
-#USE AccountDetailsServer;
+USE AccountDetailsServer;
 CREATE PROCEDURE getAccountDetailsByUsername(IN inputUsername varchar (50))
 BEGIN
 	SELECT * FROM account WHERE userName = inputUsername;
@@ -12,7 +12,7 @@ END $$
 DELIMITER ;
 
 
-#USE AccountDetailsServer;
+USE AccountDetailsServer;
 DROP procedure IF EXISTS `getAccountHoldingsById`;
 
 DELIMITER $$
@@ -22,6 +22,96 @@ DELIMITER $$
 CREATE PROCEDURE getAccountHoldingsById(IN accountInputId INT)
 BEGIN
 	SELECT * FROM account WHERE accountId = accountInputId;
+END $$
+
+DELIMITER ;
+
+
+USE AccountDetailsServer;
+DROP procedure IF EXISTS `addAccountAvailableCash`;
+
+DELIMITER $$
+
+#USE AccountDetailsServer;  uncomment if need
+
+CREATE PROCEDURE addAccountAvailableCash(IN accountInputId INT , IN accountValueAdd INT)
+BEGIN
+	Update account SET availableCash = availableCash + accountValueAdd WHERE accountId = accountInputId;
+END $$
+
+DELIMITER ;
+
+
+USE AccountDetailsServer;
+DROP procedure IF EXISTS `minusAccountAvailableCash`;
+
+DELIMITER $$
+
+#USE AccountDetailsServer;  uncomment if need
+
+CREATE PROCEDURE minusAccountAvailableCash(IN accountInputId INT , IN accountValueMinus INT)
+BEGIN
+	Update account SET availableCash = availableCash - accountValueMinus WHERE accountId = accountInputId;
+END $$
+
+DELIMITER ;
+
+
+USE AccountDetailsServer;
+DROP procedure IF EXISTS `addAccountSecurityValue`;
+
+DELIMITER $$
+
+#USE AccountDetailsServer;  uncomment if need
+
+CREATE PROCEDURE addAccountSecurityValue(IN accountInputId INT , IN accountValueAdd INT)
+BEGIN
+	Update account SET totalSecurityValue = totalSecurityValue + accountValueAdd WHERE accountId = accountInputId;
+END $$
+
+DELIMITER ;
+
+
+USE AccountDetailsServer;
+DROP procedure IF EXISTS `minusAccountSecurityValue`;
+
+DELIMITER $$
+
+#USE AccountDetailsServer;  uncomment if need
+
+CREATE PROCEDURE minusAccountSecurityValue(IN accountInputId INT , IN accountValueMinus INT)
+BEGIN
+	Update account SET totalSecurityValue = totalSecurityValue - accountValueMinus WHERE accountId = accountInputId;
+END $$
+
+DELIMITER ;
+
+
+USE AccountDetailsServer;
+DROP procedure IF EXISTS `addAccountValue`;
+
+DELIMITER $$
+
+#USE AccountDetailsServer;  uncomment if need
+
+CREATE PROCEDURE addAccountValue(IN accountInputId INT , IN accountValueAdd INT)
+BEGIN
+	Update account SET totalAccountValue = totalAccountValue + accountValueAdd WHERE accountId = accountInputId;
+END $$
+
+DELIMITER ;
+
+
+USE AccountDetailsServer;
+DROP procedure IF EXISTS `minusAccountValue`;
+
+DELIMITER $$
+
+#USE AccountDetailsServer;  uncomment if need
+
+CREATE PROCEDURE minusAccountValue(IN accountInputId INT , IN accountValueMinus INT)
+BEGIN
+	Update account SET totalAccountValue = totalAccountValue - accountValueMinus WHERE accountId = accountInputId;
 END $$
 
 DELIMITER ;
